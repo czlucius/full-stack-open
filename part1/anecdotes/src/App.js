32c58f251af270ad45acf681eb8e6865274c0538
Lucius C <58442255+czlucius@users.noreply.github.com>
2022-07-19 16:52:56 +0800

    1.12 and 1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import "./App.css"
 
-function App() {
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(getRandomInt(anecdotes.length))
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const selectHandler = () => {
+    setSelected(getRandomInt(anecdotes.length))
+  }
+  const voteHandler = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p className='text'>{anecdotes[selected]}</p>
+      <p>Votes: {votes[selected]}</p>
+      
+      <button onClick={selectHandler}>Next anecdote</button>
+      <button onClick={voteHandler}>Vote</button>
     </div>
-  );
+  )
 }
 
-export default App;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max)
+}
+
+
+
+
+
+export default App
