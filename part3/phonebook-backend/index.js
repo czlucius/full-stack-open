5314ac0bff5789e24d0a31bc7ef67fd7ddf3e93a
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2022-10-03 15:18:27 +0800

    Update files

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,8 +2,10 @@ const express = require("express")
 const morgan = require("morgan")
 
 const app = express()
+const cors = require('cors')
 app.use(express.json())
-
+app.use(cors())
+app.use(express.static('build'))
 
 function isObjEmpty(obj) {
     for (const i in obj) return false;
