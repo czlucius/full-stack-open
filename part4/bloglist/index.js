47057561b803b6d99498a4dbe1905a62dbd36dd4
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-04-17 22:12:05 +0800

    Remove server code from index.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,32 +1,17 @@
-import logger from "./utils/logger";
+const logger = require("./utils/logger")
 const app = require("./app")
 const http = require('http')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const express = require("express")
 
-const {blogsRouter} = require("./controllers/blogs")
+const blogsRouter = require("./controllers/blogs")
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.use("/api/notes", blogsRouter)
-
-
-const PORT = 3003
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
 })
 
