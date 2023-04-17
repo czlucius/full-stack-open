29bd50c081159068b11faac5be1ef393aef45281
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-04-17 22:09:47 +0800

    Move express app back to app.js
    DB must be initialised

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,4 +1,34 @@
-const express = require('express')
+const logger = require("./utils/logger")
+const http = require('http')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const express = require("express")
+const blogsRouter = require("./controllers/blogs")
 const app = express()
 
-export default app
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/blogs", blogsRouter)
+
+
+const PORT = 3003
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
+})
+
+
+
+module.exports = app
