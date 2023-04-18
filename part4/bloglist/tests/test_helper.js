2b89915ec6833765f7f1fc08263f12646a7610f6
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-04-18 11:31:02 +0800

    Add test helper

---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,20 @@
+const Blog = require("../models/blog")
+
+const initialBlogs = require("./blogs.json")
+
+async function blogsInDb() {
+    const blogs = Blog.find({})  
+    return blogs.map(blog => blog.toJSON()) // We are using the toJSON method so the _id and other fields won't be there.
+}
+
+const nonExistingId = async () => {
+    const blog = new Blog({ content: 'willremovethissoon' })
+    await blog.save()
+    await blog.deleteOne()
+  
+    return blog._id.toString()
+}
+
+module.exports = {
+    initialBlogs, blogsInDb, nonExistingId
+}
